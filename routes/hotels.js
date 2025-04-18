df38ed5f670fd560fc0a7aefbc9f7ece9b0b7ad1
CHUBAAAA <kirana_chaboo_@hotmail.com>
2025-04-18 12:57:54 +0700

    Authen

---
+++ routes/hotels.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {getHotels, getHotel, createHotel, updateHotel, deleteHotel} = require('../controllers/hotels');
+const router = express.Router();
+
+const {protect, authorize} = require('../middleware/auth');
+
+const app = express();
+
+router.route('/').get(getHotels).post(protect, authorize('admin'), createHotel);
+router.route('/:id').get(getHotel).put(protect, authorize('admin'), updateHotel).delete(protect, authorize('admin'), deleteHotel);
+
+module.exports = router;
